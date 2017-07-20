a11ab332bbce9a8a8037fc6622ecb6b0283dc74a
Antoine <antoine@delaunay.org>
2017-07-20 23:35:34 +0000

    better file naming strategy

--- rest_backend/config_sample.js
+++ rest_backend/config_sample.js
@@ -2,5 +2,6 @@ var config = {};
 
 config.root_app = '/';
 config.root_csv_directory = 'hours';
+config.root_ods_directory= 'invoices';
 
 module.exports = config;
--- rest_backend/index.js
+++ rest_backend/index.js
@@ -53,11 +53,11 @@ app.post('/greendrop/save', function (req, res) {
       if (err) { 
         if (!logNonConstraintError(err)) {
           db.prepare("update clients set occurences = occurences+1 where name = ?").run(clientName, function(err) {
-      if (err) { 
-        console.error(err);
-      }});
+            if (err) { 
+              console.error(err);
+            }});
 
-          
+
         }
       } else {
         console.log('new client saved : '+clientName);
@@ -107,7 +107,14 @@ app.post('/greendrop/save', function (req, res) {
       fs.mkdirSync(directory);
     }
     csv.stringify(worksheet.furnituresArray, {quotedString:true}, function(err, csv) {
-      var filename = directory+'/'+encodeURIComponent(worksheet.clientName).replace(/_/g,'-')+'_'+worksheet.hours+'h_'+now.getUTCFullYear()+twoCharsInteger(1+now.getUTCMonth())+twoCharsInteger(now.getUTCDate())+"_"+((seedUuid++).toString(36))+".csv"; 
+      var filename = format("%s/%s%s%s_%s_%sh_%s.csv",
+                            directory,
+                            now.getUTCFullYear(),
+                            twoCharsInteger(1+now.getUTCMonth()),
+                            twoCharsInteger(now.getUTCDate()),
+                            encodeURIComponent(worksheet.clientName).replace(/_/g,'-'),
+                            worksheet.hours,
+                           (seedUuid++).toString(36)); 
       var filename_toinvoice=filename+".todo";
       fs.writeFile(filename, csv, {encoding:'utf8'}, function(err) {
         if (err) {
@@ -152,6 +159,11 @@ const invoice_footer='invoice_footer.xmlfragment';
 app.post('/greendrop/tasks/invoices', function(req,res){
   console.log('generating invoices');
   var directory = config.root_csv_directory;
+    var now = new Date();
+  var ods_directory= config.root_ods_directory+"/"+now.getUTCFullYear();
+    if (!fs.existsSync(ods_directory)) {
+      fs.mkdirSync(ods_directory);
+    }
   var rowTemplate=fs.readFileSync('row.template', {encoding:'utf8'});
   var emptyRowTemplate=fs.readFileSync('empty_row.template', {encoding:'utf8'});
 
@@ -166,7 +178,7 @@ app.post('/greendrop/tasks/invoices', function(req,res){
             if (todoFile.endsWith(todoSuffix)) {
               console.log('found '+todoFile)
               var file = todoFile.substring(0,todoFile.length-todoSuffix.length);
-              var ods_content = file+".content";
+              var ods_content = ods_directory+"/"+file_value+".invoice";
               rmdir(ods_content,function(err){
                 if (err) {
                   console.error('could not process '+file);
@@ -181,62 +193,66 @@ app.post('/greendrop/tasks/invoices', function(req,res){
 
                       var rowIndex = 0;
                       csv.parse(fs.readFileSync(file, {encoding:'utf8'}), function(err,invoiceData){
-                    if (err) {
-                      console.error('could not process '+file);
-                      console.error(err);
-                    } else {
-                        //generating the row containing data
-                        invoiceData.forEach(function(line_value,i){
-                        currentRow = row_start_index+(rowIndex++);
-                          var row = rowTemplate
+                        if (err) {
+                          console.error('could not process '+file);
+                          console.error(err);
+                        } else {
+                          //generating the row containing data
+                          invoiceData.forEach(function(line_value,i){
+                            currentRow = row_start_index+(rowIndex++);
+                            var row = rowTemplate
                             .replace('$name', line_value[0])
                             .replace('$quantity', line_value[1])
                             .replace(/\$rowIndex/g,currentRow);
-                          fs.appendFileSync(file_rows, row, {encoding:'utf8'});
+                            fs.appendFileSync(file_rows, row, {encoding:'utf8'});
 
-                        });
-                        //filling with empty rows
-                        while(rowIndex<=nb_lines_in_invoice) {
-                          var empty_row = emptyRowTemplate
+                          });
+                          //filling with empty rows
+                          while(rowIndex<=nb_lines_in_invoice) {
+                            var empty_row = emptyRowTemplate
                             .replace(/\$rowIndex/g,row_start_index+(rowIndex++));
-                          fs.appendFileSync(file_rows, empty_row, {encoding:'utf8'});
-                        }
+                            fs.appendFileSync(file_rows, empty_row, {encoding:'utf8'});
+                          }
 
-                        //appending the footer
-                        fs.appendFileSync(file_rows, fs.readFileSync(invoice_footer, {encoding:'utf8'}), {encoding:'utf8'});
+                          //appending the footer
+                          fs.appendFileSync(file_rows, fs.readFileSync(invoice_footer, {encoding:'utf8'}), {encoding:'utf8'});
 
-                        //deleting existing archive if any
-                        var ods_file=file_value.substring(0,file_value.length-todoSuffix.length-".csv".length)+".ods";
-                        if (fs.existsSync(month+'/'+ods_file)) {
-                          console.log('deleting existing ods '+month+'/'+ods_file);
-                          fs.unlinkSync(month+'/'+ods_file);
-                        }
+                          //deleting existing archive if any
+                          var ods_file=file_value.substring(0,file_value.length-todoSuffix.length-".csv".length)+".ods";
+                          if (fs.existsSync(month+'/'+ods_file)) {
+                            console.log('deleting existing ods '+month+'/'+ods_file);
+                            fs.unlinkSync(month+'/'+ods_file);
+                          }
 
-                        //replacing variables that were part of the header
-                        var name_split=ods_file.split('_');
-                        console.log(name_split);
-                        exec.execSync(format("sed -i -e 's/#client_name/%s/' -e 's/#client_street/%s/' -e 's/#client_city/%s/' -e 's/#date_work/%s/' -e 's/#fact_num/%s/' -e 's/#date_due/%s/' content.xml", name_split[0], '', '', name_split[2], '', ''), {cwd:ods_content});
+                          //replacing variables that were part of the header
+                          var name_split=ods_file.split('_');
+                          console.log(name_split);
+                          exec.execSync(format("sed -i -e 's/#client_name/%s/' -e 's/#client_street/%s/' -e 's/#client_city/%s/' -e 's/#date_work/%s/' -e 's/#fact_num/%s/' -e 's/#date_due/%s/' content.xml",
+                                               name_split[1], 
+                                               '', 
+                                               '', 
+                                               name_split[0], 
+                                               '', 
+                                               ''), {cwd:ods_content});
 
-                        //zipping the ods
-                        //putting mimetype first in the archive to respect the ODF format and avoid a 'corrupted' warning
-                        exec.exec("zip -r ../"+safe_filename(ods_file)+" mimetype .",
-                             {cwd:ods_content},
-                            function(error, stdout, stderr){
-                              if (error) {
+                          //zipping the ods
+                          //putting mimetype first in the archive to respect the ODF format and avoid a 'corrupted' warning
+                          exec.exec("zip -r ../"+safe_filename(ods_file)+" mimetype .", {cwd:ods_content}, function(error, stdout, stderr){
+                            if (error) {
                               console.error(error);
                               console.error(stderr);
-                              } else {
-                        console.log('creating '+month+'/'+ods_file);
-                        fs.unlinkSync(todoFile);
-              rmdir(ods_content,function(err){
-                if (err) {
-                  console.error('could not process '+file);
-                  console.error(err);
-                }
-              });
-                              }
-                            });
-                    }
+                            } else {
+                              console.log('creating '+ods_directory+'/'+ods_file);
+                              fs.unlinkSync(todoFile);
+                              rmdir(ods_content,function(err){
+                                if (err) {
+                                  console.error('could not process '+file);
+                                  console.error(err);
+                                }
+                              });
+                            }
+                          });
+                        }
                       });
                     }
                   });
@@ -255,7 +271,7 @@ function safe_filename(filename) {
   return filename.replace (/'/g, '\\\'')
   .replace(/ /g,'\\ ')
   .replace(/\//g,'\\/')
-  ;
+    ;
 }
 
 
