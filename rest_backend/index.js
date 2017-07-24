ba798c81a32d41fee1824054c14a4dd91d05c4ac
Antoine Delaunay <antoine@delaunay.org>
2017-07-24 17:08:04 +0200

    using the database content to complete the invoices
    better handling of the escaped characters in clients and furnitures
    names
    small corrections to the invoice template

--- rest_backend/index.js
+++ rest_backend/index.js
@@ -35,7 +35,7 @@ function logNonConstraintError(err) {
 app.post('/greendrop/save', function (req, res) {
 
   var worksheet = {};
-  var clientName = req.body.client_name;
+  var clientName = unescape(req.body.client_name).replace(/_/g,'-');
   if (!clientName.trim()) {
     res.statusCode=500;
     res.send('empty client name, cannot save hours');
@@ -69,9 +69,10 @@ app.post('/greendrop/save', function (req, res) {
     if (isArray(req.body.furniture)) {
       req.body.furniture.forEach(function(value,i) {
         if (value.trim()) {
-          worksheet.furnitures.push({name:value, quantity:req.body.quantity[i]});
-          worksheet.furnituresArray.push([value, req.body.quantity[i]]);
-          furnitureInsert.run(value, function(err) {
+
+          worksheet.furnitures.push({name:unescape(value), quantity:req.body.quantity[i]});
+          worksheet.furnituresArray.push([unescape(value), req.body.quantity[i]]);
+          furnitureInsert.run(unescape(value), function(err) {
             if (err) { 
               logNonConstraintError(err);
             } else {
@@ -82,9 +83,9 @@ app.post('/greendrop/save', function (req, res) {
       });
     } else {
       if (req.body.furniture.trim()) {
-        worksheet.furnitures.push({name:req.body.furniture, quantity:req.body.quantity});
-        worksheet.furnituresArray.push([req.body.furniture, req.body.quantity]);
-        furnitureInsert.run(req.body.furniture, function(err) {
+        worksheet.furnitures.push({name:unescape(req.body.furniture), quantity:req.body.quantity});
+        worksheet.furnituresArray.push([unescape(req.body.furniture), req.body.quantity]);
+        furnitureInsert.run(unescape(req.body.furniture), function(err) {
           if (err) { 
             logNonConstraintError(err);
           } else {
@@ -112,7 +113,7 @@ app.post('/greendrop/save', function (req, res) {
                             now.getUTCFullYear(),
                             twoCharsInteger(1+now.getUTCMonth()),
                             twoCharsInteger(now.getUTCDate()),
-                            encodeURIComponent(worksheet.clientName).replace(/_/g,'-'),
+                            worksheet.clientName,
                             worksheet.hours,
                            (seedUuid++).toString(36)); 
       var filename_toinvoice=filename+".todo";
@@ -172,6 +173,10 @@ app.post('/greendrop/tasks/invoices', function(req,res){
     }
   var rowTemplate=fs.readFileSync('row.template', {encoding:'utf8'});
   var emptyRowTemplate=fs.readFileSync('empty_row.template', {encoding:'utf8'});
+                          db.all("SELECT * from furnitures",  function(err, furnitures) {
+                            if (err) {
+                              res.send(500,err);
+                            } else {
 
   fs.readdirSync(directory).forEach(function(year_value,i) {
     var year=directory+"/"+year_value;
@@ -206,9 +211,13 @@ app.post('/greendrop/tasks/invoices', function(req,res){
                           //generating the row containing data
                           invoiceData.forEach(function(line_value,i){
                             currentRow = row_start_index+(rowIndex++);
+                            var furniture_name = line_value[0];
+                            var unit_price = findPriceOf(furniture_name, furnitures);
                             var row = rowTemplate
-                            .replace('$name', line_value[0])
+                            .replace('$name', unescape(line_value[0]))
                             .replace('$quantity', line_value[1])
+                            .replace('$unit_price', unit_price)
+                            .replace('$unit_price_in_eur', unit_price)
                             .replace(/\$rowIndex/g,currentRow);
                             fs.appendFileSync(file_rows, row, {encoding:'utf8'});
 
@@ -233,19 +242,27 @@ app.post('/greendrop/tasks/invoices', function(req,res){
                           //replacing variables that were part of the header
                           var name_split=ods_file.split('_');
                           console.log(name_split);
-                          var client_name= decodeURIComponent(name_split[1]);
+                          var client_name= name_split[1];
                           db.all("SELECT * from clients where name =?", client_name, function(err, rows) {
                             if (err) {
                               res.send(500,err);
                             } else {
                               var client_data = rows[0];
+                              if (client_data) {
+                                //adding months to get the due date
+                                var due_date = date_of(name_split[0]);
+                                due_date.setMonth(due_date.getMonth() +config.nb_month_before_due_date);
                           exec.execSync(format("sed -i -e 's/#client_name/%s/' -e 's/#client_street/%s/' -e 's/#client_city/%s/' -e 's/#date_work/%s/' -e 's/#fact_num/%s/' -e 's/#date_due/%s/' content.xml",
                                                client_data.invoice_name,
                                                client_data.invoice_street,
                                                client_data.invoice_city,
-                                               name_split[0], 
-                                               client_data.occurences, 
-                                               ''), {cwd:ods_content});
+                                               format_invoice_date(date_of(name_split[0])), 
+                                               'facture num '+client_data.occurences, 
+                                               format_invoice_date(due_date) 
+                                               ), {cwd:ods_content});
+                              } else {
+                              console.error('no client data found for %s', client_name);
+                              }
 
                           //zipping the ods
                           //putting mimetype first in the archive to respect the ODF format and avoid a 'corrupted' warning
@@ -281,8 +298,33 @@ app.post('/greendrop/tasks/invoices', function(req,res){
       });
     }
   })
+
   res.send('ok');
+                            }
+                          });
 });
+
+function date_of(date_string) {
+  return new Date(date_string.substring(0,4), date_string.substring(4,6),date_string.substring(6,8));
+}
+
+function format_invoice_date(invoice_date) {
+  return format("%s\\/%s\\/%s",
+                invoice_date.getUTCFullYear(), 
+                twoCharsInteger(1+invoice_date.getUTCMonth()), 
+                twoCharsInteger(invoice_date.getUTCDate()));
+}
+
+function findPriceOf(furniture_name, all_furnitures) {
+  for (var i = 0; i<all_furnitures.length;i++){
+    if (all_furnitures[i].name === furniture_name) {
+      console.log('price %s found for %s', all_furnitures[i].unit_price, furniture_name);
+    return all_furnitures[i].unit_price;
+    }
+  }
+      console.log('no price found for %s', furniture_name);
+  return '0';
+}
 
 function safe_filename(filename) {
   return filename.replace (/'/g, '\\\'')
@@ -309,15 +351,50 @@ app.get('/greendrop/clients', function (req,res) {
   });
 });
 
-app.get('/greendrop/clients/:id', function (req,res) {
+//admin resources
+
+app.get('/greendrop/admin', function (req,res) {
+        res.send(format("<html><body><h1>invoices</h1><ul><li><a href='/invoices'>invoices</a></li>"+
+                 "<li><form method='post' action='%s/tasks/invoices'><input type='submit' value='generate invoices'></form></li>"+
+                 "</ul><h1>configuration</h1><a href='%s/admin/clients'>clients list</a><br><a href='%s/admin/furnitures'>furnitures list</a>",
+                       config.root_app,
+                       config.root_app,
+                       config.root_app
+                       ));
+});
+
+function listNamedEntities(entityType,res) {
+  db.serialize(function() {
+    db.all("SELECT name from "+entityType, function(err, rows) {
+      if (err) {
+        res.send(500,err);
+      } else {
+        res.send(rows.map(function(entity){return format("<li><a href='%s/admin/%s/%s'>%s</a></li>",
+                                                         config.root_app,
+                                                         entityType, 
+                                                         entity.name, 
+                                                         entity.name );})
+                      .reduce(function(list, entry){return list+entry}, '<a href=".">back to main page</a>'));
+      }
+    });
+  });
+}
+
+app.get('/greendrop/admin/clients', function (req,res) {
+  listNamedEntities('clients', res);
+});
+
+app.get('/greendrop/admin/clients/:id', function (req,res) {
   db.serialize(function() {
     db.all("SELECT * from clients where name =?", req.params.id, function(err, rows) {
       if (err) {
         res.send(500,err);
       } else {
         var client = rows[0];
-        res.send(format("<html><body><h2>client data for %s</h2><form method='POST' ><label>invoice name<br><input name='invoice_name' value='%s'></label><br><label>invoice street<br><input name='invoice_street' value='%s'></label><br><label> postal code and city<br><input name='invoice_city' value='%s'></label><br><input type='submit'></form>",
+        res.send(format("<html><body><body><a href='%s/admin/clients'>back to clients list</a><h2>client data for %s</h2><p>Current invoice number : %s</p><form method='POST' ><label>invoice name<br><input name='invoice_name' value='%s'></label><br><label>invoice street<br><input name='invoice_street' value='%s'></label><br><label> postal code and city<br><input name='invoice_city' value='%s'></label><br><input type='submit'></form>",
+                config.root_app,
                 req.params.id,
+                client.occurences,
                 client.invoice_name,
                 client.invoice_street,
                 client.invoice_city));
@@ -326,7 +403,7 @@ app.get('/greendrop/clients/:id', function (req,res) {
   });
 });
 
-app.post('/greendrop/clients/:id', function (req,res) {
+app.post('/greendrop/admin/clients/:id', function (req,res) {
           db.prepare("update clients set invoice_name= ?, invoice_street=?, invoice_city=? where name = ?").run(
             req.body.invoice_name,
             req.body.invoice_street,
@@ -339,9 +416,45 @@ app.post('/greendrop/clients/:id', function (req,res) {
             res.redirect('./'+req.params.id);
 });
 
-if (!fs.existsSync(config.root_csv_directory)) {
-  fs.mkdirSync(config.root_csv_directory);
+app.get('/greendrop/admin/furnitures', function (req,res) {
+  listNamedEntities('furnitures', res);
+});
+
+app.get('/greendrop/admin/furnitures/:id', function (req,res) {
+  db.serialize(function() {
+    db.all("SELECT * from furnitures where name =?", req.params.id, function(err, rows) {
+      if (err) {
+        res.send(500,err);
+      } else {
+        var client = rows[0];
+        res.send(format("<html><body><a href='%s/admin/furnitures'>back to furnitures list</a><h2>parameters for furniture %s</h2><form method='POST' ><label>unit price<br><input name='unit_price' value='%s'></label><br><input type='submit'></form>",
+                        config.root_app,
+                req.params.id,
+                client.unit_price));
+      }
+    });
+  });
+});
+
+app.post('/greendrop/admin/furnitures/:id', function (req,res) {
+          db.prepare("update furnitures set unit_price = ? where name = ?").run(
+            req.body.unit_price,
+            req.params.id, 
+            function(err) {
+            if (err) { 
+              console.error(err);
+            }});
+            res.redirect('./'+req.params.id);
+});
+
+function createConfigDirectories(directory) {
+if (!fs.existsSync(directory)) {
+  console.log('creating '+directory);
+  fs.mkdirSync(directory);
 }
+}
+createConfigDirectories(config.root_csv_directory);
+createConfigDirectories(config.root_ods_directory);
 
 db.run("CREATE TABLE if not exists clients (name TEXT primary key, invoice_name TEXT, invoice_street TEXT, invoice_city TEXT, occurences INTEGER)");
 db.run("CREATE TABLE if not exists furnitures (name TEXT primary key, unit_price TEXT)");
